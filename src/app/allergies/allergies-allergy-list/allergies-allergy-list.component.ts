d7d1853f532cdbddf0f85b02d4ba0c26e92ad602
Alejandro Lopez Osornio <alopez72@gmail.com>
2024-03-08 11:11:08 -0300

    Setting category to lowercase

--- src/app/allergies/allergies-allergy-list/allergies-allergy-list.component.ts
+++ src/app/allergies/allergies-allergy-list/allergies-allergy-list.component.ts
@@ -209,6 +209,7 @@ export class AllergiesAllergyListComponent  implements OnInit, OnChanges {
     this.outputAllergy.verificationStatus.coding = [this.selectedVerificationStatus];
     this.outputAllergy.type = (this.selectedIntoleranceType) ? this.selectedIntoleranceType.fhirCode : '';
     this.outputAllergy.category = (this.selectedIntoleranceCategories.length) ? this.selectedIntoleranceCategories.map((option: any) => option.display) : [];
+    this.outputAllergy.category = this.outputAllergy.category.lowercase();
     this.outputAllergy.criticality = (this.selectedCriticality?.code) ? [this.selectedCriticality.code] : {};
     this.outputAllergy.reaction = [];
     this.selectedReactions.forEach((reaction: any) => {
